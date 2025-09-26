c2f51e8f6f7d564e57e59420ab1e1e1f793a7ebf
wkhabou3 <wkhabou33@gmail.com>
2025-09-26 12:52:34 -0400

    changes after watching lecture 10

--- Hospital.sql
+++ Hospital.sql
@@ -1,9 +1,12 @@
+create database hospital;
+use hospital;
+
 create table person (
 	ssn decimal(10, 0) not null,
-    fname char(100) not null,
-    lname char(100) not null,
+    fname varchar(100) not null,
+    lname varchar(100) not null,
     bdate date not null,
-    addr char(100) not null,
+    addr varchar(100) not null,
     primary key (ssn)
 );
 
@@ -11,9 +14,9 @@ create table patient (
 	ssn decimal(10, 0) not null,
     contact decimal(10, 0) not null, 
     funds int not null,
+    check (funds >= 0),
     primary key (ssn),
     foreign key (ssn) references person(ssn)
-    on update restrict
     on delete cascade
 );
 
@@ -22,28 +25,28 @@ create table staff (
 	ssn decimal(10, 0) not null,
     salary int not null,
     hireDate date not null,
+    check (staffID > 0 and salary >= 0),
     primary key (staffID),
     foreign key (ssn) references person(ssn)
-    on update restrict
     on delete cascade
 );
 
 create table doctor (
 	licenseNumber int not null,
     id int not null,
-    experience tinyint not null,
+    experience int not null,
+    check (licenseNumber > 0 and id > 0 and experience >= 0),
     primary key (licenseNumber),
     foreign key (id) references staff(staffID)
-    on update restrict
     on delete cascade
 );
 
 create table nurse (
 	nurseID int not null,
     regExpiration date not null,
-    shiftType char(100) not null,
+    shiftType varchar(100) not null,
+    check (nurseID > 0),
     primary key (nurseID),
     foreign key (nurseID) references staff(staffID)
-    on update restrict
     on delete cascade
 );
